2db6dd48e411cf36242e1194576d43ce5e5252c7
Yanis Benyacine <sterllytb@gmail.com>
2024-10-11 22:26:28 +0200

    ci:Add new ParticleLib

--- src/main/java/fr/yanis/superbecorpweapons/utils/ParticleLib.java
+++ src/main/java/fr/yanis/superbecorpweapons/utils/ParticleLib.java
@@ -2,14 +2,19 @@ package fr.yanis.superbecorpweapons.utils;
 
 import com.destroystokyo.paper.ParticleBuilder;
 import fr.yanis.superbecorpweapons.SCWMain;
+import fr.yanis.superbecorpweapons.item.management.Item;
+import org.bukkit.Bukkit;
 import org.bukkit.Color;
 import org.bukkit.Location;
 import org.bukkit.Particle;
+import org.bukkit.entity.Player;
 import org.bukkit.scheduler.BukkitRunnable;
+
+import java.util.Collection;
 
 public class ParticleLib {
 
-    public static void spawnDome(Location location, Color color, double maxRadius) {
+    public static void spawnDome(Location location, Color color, double maxRadius, Item item) {
         new BukkitRunnable() {
             double radius = 0;
             @Override
@@ -18,6 +23,7 @@ public class ParticleLib {
                     this.cancel();
                     return;
                 }
+                Collection<? extends Player> players = Bukkit.getOnlinePlayers();
                 for (double y = 0; y <= radius; y += 0.2) {
                     double currentRadius = Math.sqrt(radius * radius - y * y);
                     for (int i = 0; i < 360; i += 10) {
@@ -30,6 +36,12 @@ public class ParticleLib {
                                 .location(location)
                                 .count(2)
                                 .spawn();
+                        for (Player player : players) {
+                            if (player.getWorld().equals(location.getWorld())
+                                    && player.getLocation().distanceSquared(location) <= 0.25) {
+                                item.whenPlayerIsTouchedByParticle(player);
+                            }
+                        }
                         location.subtract(x, y, z);
                     }
                 }
@@ -38,7 +50,7 @@ public class ParticleLib {
         }.runTaskTimer(SCWMain.getInstance(), 0L, 0L);
     }
 
-    public static void spawnCircleAt(Location location, Color color){
+    public static void spawnCircleAt(Location location, Color color, Item item){
         new BukkitRunnable() {
             double radius = 0;
             @Override
@@ -47,6 +59,7 @@ public class ParticleLib {
                     this.cancel();
                     return;
                 }
+                Collection<? extends Player> players = Bukkit.getOnlinePlayers();
                 for (int i = 0; i < 360; i += 10) {
                     double angle = Math.toRadians(i);
                     double x = radius * Math.cos(angle);
@@ -57,6 +70,12 @@ public class ParticleLib {
                             .location(location)
                             .count(2)
                             .spawn();
+                    for (Player player : players) {
+                        if (player.getWorld().equals(location.getWorld())
+                                && player.getLocation().distanceSquared(location) <= 0.25) {
+                            item.whenPlayerIsTouchedByParticle(player);
+                        }
+                    }
                     location.subtract(x, 0, z);
                 }
                 radius += 0.2;

311d77eed097fb768bb3490e7dd56ab7482cc9bd
Yanis Benyacine <sterllytb@gmail.com>
2024-10-11 19:16:47 +0200

    feat:API - getInstance

--- src/main/java/fr/yanis/superbecorpweapons/SCWMain.java
+++ src/main/java/fr/yanis/superbecorpweapons/SCWMain.java
@@ -4,14 +4,19 @@ import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SCWMain extends JavaPlugin {
 
+    public static SCWMain instance;
+
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        instance = this;
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static SCWMain getInstance() {
+        return instance;
     }
 }

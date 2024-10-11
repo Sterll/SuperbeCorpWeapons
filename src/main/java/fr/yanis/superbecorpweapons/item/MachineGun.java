ab9c7bbf6a9420ccd36d960b52c102d223d0f6f5
Yanis Benyacine <sterllytb@gmail.com>
2024-10-11 20:42:34 +0200

    feat:Machine Gun

--- src/main/java/fr/yanis/superbecorpweapons/item/MachineGun.java
+++ src/main/java/fr/yanis/superbecorpweapons/item/MachineGun.java
@@ -4,9 +4,12 @@ import fr.yanis.superbecorpweapons.item.management.Item;
 import fr.yanis.superbecorpweapons.utils.ItemBuilder;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
+import org.bukkit.World;
+import org.bukkit.entity.Arrow;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.util.Vector;
 
 public class MachineGun extends Item {
     @Override
@@ -34,11 +37,22 @@ public class MachineGun extends Item {
 
     @Override
     public void onUse(PlayerInteractEvent e) {
+        e.getPlayer().sendMessage("§bOUI ! ");
+        int amountOfShoot = 5;
+        double spread = 0.1;
 
-    }
+        for (int i = 0; i < amountOfShoot; i++) {
+            Arrow arrow = e.getPlayer().launchProjectile(Arrow.class);
+            arrow.setVelocity(
+                    arrow.getVelocity().add(new Vector(
+                            Math.random() * spread - spread / 2,
+                            Math.random() * spread - spread / 2,
+                            Math.random() * spread - spread / 2
+                    ))
+            );
+            arrow.setDamage(1.0);
+            arrow.setCritical(false);
 
-    @Override
-    public void onUseAtEntity(PlayerInteractAtEntityEvent e) {
-
+        }
     }
 }

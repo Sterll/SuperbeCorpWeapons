72e9061f053aff92dccd1a58c3cc277c272fdeb2
Yanis Benyacine <sterllytb@gmail.com>
2024-10-11 19:24:07 +0200

    feat:Add Item Event

---
+++ src/main/java/fr/yanis/superbecorpweapons/ItemsEvents.java
@@ -0,0 +1,28 @@
+package fr.yanis.superbecorpweapons;
+
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
+
+public class ItemsEvents implements Listener {
+
+    @EventHandler
+    public void onPlayerInteract(PlayerInteractEvent event) {
+        if(event.getItem() == null) return;
+        ItemManager.getItems().forEach((item, itemManager) -> {
+            if(event.getItem() == item) {
+                itemManager.item().onUse(event);
+            }
+        });
+    }
+
+    @EventHandler
+    public void onPlayerInteractAtEntity(PlayerInteractAtEntityEvent event){
+        ItemManager.getItems().forEach((item, itemManager) -> {
+            if(event.getPlayer().getInventory().getItemInMainHand() == item) {
+                itemManager.item().onUseAtEntity(event);
+            }
+        });
+    }
+}

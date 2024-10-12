528b60629ab3ce2a7496372422d46481a54d113c
Yanis Benyacine <sterllytb@gmail.com>
2024-10-12 21:55:49 +0200

    feat:SummonersStaff ( without particles )

--- src/main/java/fr/yanis/superbecorpweapons/item/SummonersStaff.java
+++ src/main/java/fr/yanis/superbecorpweapons/item/SummonersStaff.java
@@ -4,11 +4,22 @@ import fr.yanis.superbecorpweapons.item.management.Item;
 import fr.yanis.superbecorpweapons.utils.ItemBuilder;
 import net.kyori.adventure.text.Component;
 import org.bukkit.Material;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Zombie;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class SummonersStaff extends Item {
+
+    private HashMap<Player, ArrayList<Zombie>> zombies = new HashMap<>();
+
     @Override
     public String getKey() {
         return "summoners_staff";
@@ -34,11 +45,43 @@ public class SummonersStaff extends Item {
 
     @Override
     public void onUse(PlayerInteractEvent e) {
-
+        Zombie zombie = e.getPlayer().getWorld().spawn(e.getPlayer().getLocation(), Zombie.class);
+        zombie.setCustomName("§cZombie de §b" + e.getPlayer().getName());
+        zombie.setCustomNameVisible(true);
+        zombie.setTarget(null);
+        zombie.setAdult();
+        zombie.setAI(false);
+        addZombie(e.getPlayer(), zombie);
     }
 
     @Override
-    public void onProjectileHit(ProjectileHitEvent e) {
+    public void onAttackEntity(EntityDamageByEntityEvent e) {
+        if(zombies.containsKey(e.getDamager())) {
+            for(Zombie zombie : zombies.get(e.getDamager())) {
+                zombie.setTarget((LivingEntity) e.getEntity());
+            }
+        }
+    }
 
+    @Override
+    public void onMove(PlayerMoveEvent e) {
+        if(zombies.containsKey(e.getPlayer())) {
+            for(Zombie zombie : zombies.get(e.getPlayer())) {
+                zombie.getPathfinder().moveTo(e.getPlayer());
+            }
+        }
+    }
+
+    public void addZombie(Player player, Zombie zombie) {
+        if (!zombies.containsKey(player)) {
+            zombies.put(player, new ArrayList<>());
+        }
+        zombies.get(player).add(zombie);
+    }
+
+    public void removeZombie(Player player, Zombie zombie) {
+        if (zombies.containsKey(player)) {
+            zombies.get(player).remove(zombie);
+        }
     }
 }

1f3fd73a014e7e7fa7e9d5d21a371a8768e90e67
Yanis Benyacine <sterllytb@gmail.com>
2024-10-16 00:32:30 +0200

    ci:Remove useless onMove event

--- src/main/java/fr/yanis/superbecorpweapons/item/management/Item.java
+++ src/main/java/fr/yanis/superbecorpweapons/item/management/Item.java
@@ -2,22 +2,19 @@ package fr.yanis.superbecorpweapons.item.management;
 
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
-import org.bukkit.event.entity.EntityCombustEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.player.PlayerInteractAtEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
-import org.bukkit.event.player.PlayerMoveEvent;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.scheduler.BukkitTask;
 
 import java.util.HashMap;
 
 public abstract class Item {
 
-    HashMap<Player, Boolean> cooldown = new HashMap();
-    HashMap<Player, CoolDownTask> cooldownTask = new HashMap();
+    HashMap<Player, Boolean> cooldown = new HashMap<>();
+    HashMap<Player, CoolDownTask> cooldownTask = new HashMap<>();
 
     public abstract String getKey();
     public abstract String getName();
@@ -27,7 +24,6 @@ public abstract class Item {
 
     public abstract void onUse(PlayerInteractEvent e);
     public void onProjectileHit(ProjectileHitEvent e){}
-    public void onUseAtEntity(PlayerInteractAtEntityEvent e){}
     public void onAttackEntity(EntityDamageByEntityEvent e){}
 
     public void onEntityDeath(EntityDeathEvent e){}
